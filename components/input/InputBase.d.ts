affc2cfbb3a4ceac80f8cec0824d2496e598888a
Galang Piliang <galangpiliang@gmail.com>
2021-05-07 19:17:06 +0700

    feat(export) add InputBase & InputLabel and update select

---
+++ components/input/InputBase.d.ts
@@ -0,0 +1,5 @@
+import { InputBaseProps as MuiInputBaseProps } from '@material-ui/core/InputBase';
+import * as React from 'react';
+export declare type InputBaseProps = MuiInputBaseProps;
+declare const InputBase: React.FC<InputBaseProps>;
+export default InputBase;
--- components/input/Select.d.ts
+++ components/input/Select.d.ts
@@ -1,8 +1,11 @@
+import MenuItem, { MenuItemProps as MuiMenuItemProps } from '@material-ui/core/MenuItem';
 import { SelectProps as Props } from '@material-ui/core/Select';
 import * as React from 'react';
 import { IconList } from '../icons/Icons';
 export declare type SelectProps = {
     icon?: keyof typeof IconList;
 } & Props;
+export declare type MenuItemProps = MuiMenuItemProps;
 declare const Select: React.FC<SelectProps>;
+export { Select, MenuItem };
 export default Select;
