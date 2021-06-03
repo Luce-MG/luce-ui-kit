db0ee336be5b892fd093eb2f91d83162b1f36b6f
yitaoz <yitaoz@stanford.edu>
2021-06-03 12:47:08 +0800

    resignation card update (#16)

--- components/molecules/ResignationCard.d.ts
+++ components/molecules/ResignationCard.d.ts
@@ -1,22 +1,20 @@
 import * as React from 'react';
 import { CardPopoverProps } from '../molecules/CardPopover';
 import { CustomVariant } from '../tags/Tags';
-export interface CleanerReasignation {
-    id: number;
+export interface ResignedCleaner {
     name: string;
     phone: string;
     type: string;
     nationality: string;
     lastDay?: string;
     offboardedDate?: string;
-    tag: CustomVariant;
-    tagLabel: string;
 }
 export declare type ResignationCardProps = {
-    cleaner: CleanerReasignation;
-    data: CardPopoverProps['data'];
-    hidePopover?: boolean;
-    viewCleanerDetail(id: number): void;
+    resignedCleaner: ResignedCleaner;
+    tagLabel: string;
+    labelVariant: CustomVariant;
+    onClick(): void;
+    data?: CardPopoverProps['data'];
 };
 declare const ResignationCard: React.FC<ResignationCardProps>;
 export default ResignationCard;
