805f69cf66e0f3a3e6ed56b6b32adc5a77a8fb7a
putra-cardinal <putra@cardinaldigital.com>
2022-01-10 18:15:00 +0700

    chore(fix): count total pagination

--- components/atom/NoCountTablePagination.d.ts
+++ components/atom/NoCountTablePagination.d.ts
@@ -1,18 +1,12 @@
 import * as React from 'react';
-interface NoCountTablePaginationProps {
-    dataLength: number;
+export interface NoCountTablePaginationProps {
+    dataLength?: number;
     onChange(page: number): void;
     onChangeRowsPerPage?(rows: number): void;
     rowsPerPage: number;
     rowsPerPageOptions?: number[];
     count: number;
+    page: number;
 }
-export declare type NoCountPaginationProps = {
-    count: number;
-    rowsPerPage: number;
-    onChange(page: number): void;
-    onChangeRowsPerPage?(rows: number): void;
-};
-export declare function getCount(count: number, rowsPerPage: number): number;
 declare const NoCountTablePagination: React.FC<NoCountTablePaginationProps>;
 export default NoCountTablePagination;
