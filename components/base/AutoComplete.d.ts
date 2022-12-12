cc52c5d128fa9794e0c2edd79e99f3bf29fc0943
Rizky Fathul <rizkyfh2@gmail.com>
2022-12-12 09:09:37 +0700

    chore: update autcomplete type

--- components/base/AutoComplete.d.ts
+++ components/base/AutoComplete.d.ts
@@ -3,6 +3,6 @@ import useMuiAutocomplete from '@material-ui/lab/useAutocomplete';
 import { AutocompleteChangeReason as MuiAutocompleteChangeReason } from '@material-ui/lab/useAutocomplete/useAutocomplete';
 import React from 'react';
 export declare type AutoCompleteProps<T> = Props<T, boolean | undefined, boolean | undefined, boolean | undefined>;
-export declare type AutoCompleteChangeReason = MuiAutocompleteChangeReason;
+export declare type AutocompleteChangeReason = MuiAutocompleteChangeReason;
 export declare const useAutocomplete: typeof useMuiAutocomplete;
 export declare const Autocomplete: <T extends unknown>(props: React.PropsWithChildren<AutoCompleteProps<T>>) => JSX.Element;
