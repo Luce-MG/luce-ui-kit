8ab8c7f3cc4dda08b2145e44eb6c25db7a8c1ab5
Rizky Fathul <rizkyfh2@gmail.com>
2022-12-12 11:16:00 +0700

    chore: update autocomplete type

--- components/base/AutoComplete.d.ts
+++ components/base/AutoComplete.d.ts
@@ -1,8 +1,7 @@
-import { AutocompleteProps as Props } from '@material-ui/lab/Autocomplete';
+import MuiAutocomplete, { AutocompleteProps as Props } from '@material-ui/lab/Autocomplete';
 import useMuiAutocomplete from '@material-ui/lab/useAutocomplete';
 import { AutocompleteChangeReason as MuiAutocompleteChangeReason } from '@material-ui/lab/useAutocomplete/useAutocomplete';
-import React from 'react';
-export declare type AutoCompleteProps<T> = Props<T, boolean | undefined, boolean | undefined, boolean | undefined>;
+export declare type AutoCompleteProps<T, Multiple extends boolean | undefined, DisableClearable extends boolean | undefined, FreeSolo extends boolean | undefined> = Props<T, Multiple, DisableClearable, FreeSolo>;
 export declare type AutocompleteChangeReason = MuiAutocompleteChangeReason;
 export declare const useAutocomplete: typeof useMuiAutocomplete;
-export declare const Autocomplete: <T extends unknown>(props: React.PropsWithChildren<AutoCompleteProps<T>>) => JSX.Element;
+export declare const Autocomplete: typeof MuiAutocomplete;
--- components/molecules/SkillSetAutoComplete.d.ts
+++ components/molecules/SkillSetAutoComplete.d.ts
@@ -4,7 +4,7 @@ export interface SkillSetData {
     label: string;
     value: string;
 }
-export interface SkillSetAutoCompleteProps extends AutoCompleteProps<SkillSetData> {
+export interface SkillSetAutoCompleteProps extends AutoCompleteProps<SkillSetData, true, true, undefined> {
     dataSkillSets: SkillSetData[];
 }
 export declare const SkillSetAutoComplete: React.FC<SkillSetAutoCompleteProps>;
