d549734109418622181f554b6275a7427d1f7066
jadhuk <jadhuk11@gmail.com>
2024-03-18 16:16:05 +0700

    fix: add ArrowLeftLong icon

---
+++ stories/assets/icons/ArrowLeftLongIcon.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare function SvgArrowLeftLongIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default SvgArrowLeftLongIcon;
---
+++ stories/assets/icons/ArrowLeftLongIcon.js
@@ -0,0 +1,39 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __importStar(require("react"));
+function SvgArrowLeftLongIcon(props) {
+    return (React.createElement("svg", __assign({ width: "1em", height: "1em", viewBox: "0 0 19 18", fill: "none", xmlns: "http://www.w3.org/2000/svg" }, props),
+        React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M4.414 10l6.293 6.293-1.414 1.414L.586 9 9.293.293l1.414 1.414L4.414 8H19v2H4.414z", fill: "currentColor" })));
+}
+exports.default = SvgArrowLeftLongIcon;
+//# sourceMappingURL=ArrowLeftLongIcon.js.map
